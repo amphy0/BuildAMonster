eb2e3127466503c801aa967fe9e697385918d8a7
amphy0 <106935682+amphy0@users.noreply.github.com>
2024-04-23 20:27:34 -0700

    finished

--- src/Scenes/Monster.js
+++ src/Scenes/Monster.js
@@ -25,6 +25,8 @@ class Monster extends Phaser.Scene {
     create() {
         let my = this.my;   // create an alias to this.my for readability
 
+        this.A_Key = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
+        this.D_Key = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
         // Create the main body sprite
         //
         // this.add.sprite(x,y, "{atlas key name}", "{name of sprite within atlas}")
@@ -39,20 +41,45 @@ class Monster extends Phaser.Scene {
         my.sprite.leg2 = this.add.sprite(this.bodyX-80, this.bodyY+120, "monsterParts", "leg_darkB.png");
         my.sprite.leg2.flipX = true;
         my.sprite.eye = this.add.sprite(this.bodyX, this.bodyY-30, "monsterParts", "eye_human.png");
-        my.sprite.nose = this.add.sprite(this.bodyX+50, this.bodyY, "monsterParts", "nose_red.png");
-        my.sprite.nose = this.add.sprite(this.bodyX-50, this.bodyY, "monsterParts", "nose_red.png");
+        my.sprite.nose1 = this.add.sprite(this.bodyX+50, this.bodyY, "monsterParts", "nose_red.png");
+        my.sprite.nose2 = this.add.sprite(this.bodyX-50, this.bodyY, "monsterParts", "nose_red.png");
         my.sprite.mouth1 = this.add.sprite(this.bodyX, this.bodyY+60, "monsterParts", "mouthA.png");
         my.sprite.mouth2 = this.add.sprite(this.bodyX, this.bodyY+60, "monsterParts", "mouthB.png");
         my.sprite.mouth3 = this.add.sprite(this.bodyX, this.bodyY+60, "monsterParts", "mouth_closed_happy.png");
         my.sprite.mouth1.visible = false;
         my.sprite.mouth2.visible = false;
-        
+
+        this.input.keyboard.on('keydown-S', (event) => {
+            my.sprite.mouth1.visible = true;
+            my.sprite.mouth2.visible = false;
+            my.sprite.mouth3.visible = false;
+        });
+        this.input.keyboard.on('keydown-F', (event) => {
+            my.sprite.mouth1.visible = false;
+            my.sprite.mouth2.visible = true;
+            my.sprite.mouth3.visible = false;
+        });
     }
 
     update() {
         let my = this.my;    // create an alias to this.my for readability
-
-       
+        if (this.A_Key.isDown) {
+            this.bodyX -= 2; // Move left
+        }
+        if (this.D_Key.isDown) {
+            this.bodyX += 2; // Move right
+        }
+        my.sprite.body.setPosition(this.bodyX, this.bodyY);
+        my.sprite.arm1.setPosition(this.bodyX+80, this.bodyY+40);
+        my.sprite.arm2.setPosition(this.bodyX-80, this.bodyY+40);
+        my.sprite.leg1.setPosition(this.bodyX+80, this.bodyY+120);
+        my.sprite.leg2.setPosition(this.bodyX-80, this.bodyY+120);
+        my.sprite.eye.setPosition(this.bodyX, this.bodyY-30);
+        my.sprite.nose1.setPosition(this.bodyX+50, this.bodyY);
+        my.sprite.nose2.setPosition(this.bodyX-50, this.bodyY);
+        my.sprite.mouth1.setPosition(this.bodyX, this.bodyY+60);
+        my.sprite.mouth2.setPosition(this.bodyX, this.bodyY+60);
+        my.sprite.mouth3.setPosition(this.bodyX, this.bodyY+60);
     }
 
 }
