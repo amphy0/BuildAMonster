52d0c4c1be66a7276676b00ba2b887bd9b6d0876
amphy0 <106935682+amphy0@users.noreply.github.com>
2024-04-23 19:54:24 -0700

    test

--- src/Scenes/Monster.js
+++ src/Scenes/Monster.js
@@ -32,7 +32,20 @@ class Monster extends Phaser.Scene {
         // look in spritesheet_default.xml for the individual sprite names
         // You can also download the asset pack and look in the PNG/default folder.
         my.sprite.body = this.add.sprite(this.bodyX, this.bodyY, "monsterParts", "body_greenD.png");
-
+        my.sprite.arm1 = this.add.sprite(this.bodyX+80, this.bodyY+40, "monsterParts", "arm_blueB.png");
+        my.sprite.arm2 = this.add.sprite(this.bodyX-80, this.bodyY+40, "monsterParts", "arm_blueB.png");
+        my.sprite.arm2.flipX=true;
+        my.sprite.leg1 = this.add.sprite(this.bodyX+80, this.bodyY+120, "monsterParts", "leg_darkB.png");
+        my.sprite.leg2 = this.add.sprite(this.bodyX-80, this.bodyY+120, "monsterParts", "leg_darkB.png");
+        my.sprite.leg2.flipX = true;
+        my.sprite.eye = this.add.sprite(this.bodyX, this.bodyY-30, "monsterParts", "eye_human.png");
+        my.sprite.nose = this.add.sprite(this.bodyX+50, this.bodyY, "monsterParts", "nose_red.png");
+        my.sprite.nose = this.add.sprite(this.bodyX-50, this.bodyY, "monsterParts", "nose_red.png");
+        my.sprite.mouth1 = this.add.sprite(this.bodyX, this.bodyY+60, "monsterParts", "mouthA.png");
+        my.sprite.mouth2 = this.add.sprite(this.bodyX, this.bodyY+60, "monsterParts", "mouthB.png");
+        my.sprite.mouth3 = this.add.sprite(this.bodyX, this.bodyY+60, "monsterParts", "mouth_closed_happy.png");
+        my.sprite.mouth1.visible = false;
+        my.sprite.mouth2.visible = false;
         
     }
 
